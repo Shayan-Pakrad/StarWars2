190e04097e4cd9c4981727c50f71d8aac127c436
Shayan Pakrad <shaayanpakrad@gmail.com>
2024-02-18 22:32:49 +0330

    feat:added movement

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdlib.h>
 
 using namespace std;
 
@@ -18,33 +19,101 @@ struct Banshee{
     int heal = 6;
     int size = 4;
 };
+struct Spaceship{
+    int x;
+    int y;
+};
 
-void render(int &map_size);
+void menu();
+int show_menu();
+void play_game(int &map_size, int &target_point);
+void move_spaceship_left(Spaceship &my_spaceship);
+void move_spaceship_right(Spaceship &my_spaceship);
+void render_map(int &map_size, Spaceship &my_spaceship);
+
 char cell_to_string(int value);
 
 int main(){
-    int map_size;
-    cin >> map_size;
 
-    render(map_size);
-
-
+    menu();
 }
 
-void show_menu(){
+void menu(){
+    int user_choice = show_menu();
+
+    int map_size;
+    int target_point;
+    
+    if (user_choice == 1){
+
+        do{
+            system("cls");
+            cout << "enter the size of map = ";
+            cin >> map_size;
+        }while(map_size < 15);
+
+        system("cls");
+        cout << "enter the point = ";
+        cin >> target_point;
+
+        play_game(map_size, target_point);   
+    }
+
+    else if (user_choice == 2){
+
+    }
+
+    else if (user_choice == 3){
+        exit(0);
+    }
+}
+
+int show_menu(){
     cout << "Menu:" << endl;
     cout << " 1. start" << endl;
     cout << " 2. load" << endl;
     cout << " 3. exit" << endl;
     int user_choice;
     cin >> user_choice;
+
+    return user_choice;
+}
+
+void play_game(int &map_size, int &target_point){
+
+    Spaceship my_spaceship;
+
+    my_spaceship.x = map_size - 1;
+    my_spaceship.y = (map_size - 1) / 2;
+    
+    render_map(map_size, my_spaceship);
+    
+    char spaceship_direction;
+    while (true){
+        cin >> spaceship_direction;
+
+        switch (spaceship_direction){
+            case 'a':
+                move_spaceship_left(my_spaceship); break;
+            case 'd':
+                move_spaceship_right(my_spaceship); break;
+        }
+        render_map(map_size, my_spaceship);
+    }
     
 }
 
-void render(int &map_size){
+void move_spaceship_left(Spaceship &my_spaceship){
+    my_spaceship.y--;
+}
+void move_spaceship_right(Spaceship &my_spaceship){
+    my_spaceship.y++;
+}
 
+void render_map(int &map_size, Spaceship &my_spaceship){
     int map[map_size][map_size] = {};
-    map[map_size - 1][(map_size - 1) / 2] = 1;
+    
+    map[my_spaceship.x][my_spaceship.y] = 1;
 
     for (int ifor = 0; ifor < map_size * 2; ifor++){
         if (ifor % 2 == 0){
@@ -67,7 +136,6 @@ void render(int &map_size){
         cout << " ---";
     }
     cout << endl;
-
 }
 
 char cell_to_string(int value){
